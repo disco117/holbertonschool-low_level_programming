5b22a9eaf84cfcf7cfa091aa5e23908f6aa68cbe
disco117 <3642.disco@gmail.com>
2021-10-01 17:20:16 +0000

    test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ *  Return: end the value of the program on 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")"
+		return (0);
+}
